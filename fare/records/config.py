4cf701683cfbf2c9eb684d496a156acb59a42602
Eugenio <eugeniocorso95@gmail.com>
2019-10-23 10:11:54 +0200

    feat: changed permission to create, delete and update records without files

--- fare/records/config.py
+++ fare/records/config.py
@@ -11,13 +11,14 @@ from __future__ import absolute_import, print_function
 
 from invenio_indexer.api import RecordIndexer
 from invenio_records_rest.facets import terms_filter
-from invenio_records_rest.utils import allow_all, check_elasticsearch
+from invenio_records_rest.utils import deny_all, allow_all, check_elasticsearch
 from invenio_search import RecordsSearch
 
 
 def _(x):
     """Identity function for string extraction."""
     return x
+
 
 RECORDS_REST_ENDPOINTS = {
     'recid': dict(
@@ -46,10 +47,10 @@ RECORDS_REST_ENDPOINTS = {
         default_media_type='application/json',
         max_result_window=10000,
         error_handlers=dict(),
-        create_permission_factory_imp=allow_all,
+        create_permission_factory_imp=deny_all,
         read_permission_factory_imp=check_elasticsearch,
-        update_permission_factory_imp=allow_all,
-        delete_permission_factory_imp=allow_all,
+        update_permission_factory_imp=deny_all,
+        delete_permission_factory_imp=deny_all,
         list_permission_factory_imp=allow_all
     ),
 }
