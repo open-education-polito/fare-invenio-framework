75735902640d0ba0b9a51bfdf2722648d0c1b61f
Gabriele <gabrielecorso95@gmail.com>
2020-07-30 19:05:14 +0200

    feat: new frontpage style

--- fare/config.py
+++ fare/config.py
@@ -59,7 +59,7 @@ THEME_SITENAME = _('FARE')
 #: Use default frontpage.
 THEME_FRONTPAGE = True
 #: Frontpage title.
-THEME_FRONTPAGE_TITLE = _(' ')
+THEME_FRONTPAGE_TITLE = _('Esplora. Collabora. Impara.')
 #: Frontpage template.
 THEME_FRONTPAGE_TEMPLATE = 'fare/frontpage.html'
 #: Theme logo.
