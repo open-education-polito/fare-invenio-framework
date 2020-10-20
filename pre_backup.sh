0cc4a1a48d7b734466ce1d41a462ef7516b4d03a
Gabriele <gabrielecorso95@gmail.com>
2020-10-20 10:24:33 +0200

    fix: deleted pre_backup script

--- pre_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# docker exec fare-invenio_db_1 chmod 777 /backup_fare_db
-# docker exec fare-invenio_es_1 chmod 777 /usr/share/elasticsearch/backup_fare_es
-
-chmod 777 ~/fare_shared_data
-chmod 777 ~/fare_shared_data/fare_db
-chmod 777 ~/fare_shared_data/fare_es
-chmod 777 ~/fare_shared_data/fare_files
